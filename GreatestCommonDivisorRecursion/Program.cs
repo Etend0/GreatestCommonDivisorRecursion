18be6e699b595327a2f3ea1aca02132e693dd42a
Etendo <etendo123@gmail.com>
2026-03-17 00:50:25 -0600

    Added all the challenges for the GreatestCommonDivisorRecursion.

--- GreatestCommonDivisorRecursion/Program.cs
+++ GreatestCommonDivisorRecursion/Program.cs
@@ -1,4 +1,7 @@
-﻿/*
+﻿using System.Collections;
+using System.Diagnostics;
+
+/*
  * Elijah Hodge
  * CST - 250
  * 03/16/2026
@@ -11,16 +14,76 @@
 //--------------------------------------------------------
 
 // Declare and initialize
-int number1 = 0, number2 = 0, result = 0;
+Stopwatch stopwatch = new Stopwatch(); // Create a stopwatch to track the time taken to calculate the GCD between recursion and iteration
+int number1 = 0, number2 = 0, number3 = 0, result = 0;
 
 // Call the GetInput method
-Utility.GetInput(ref number1, ref number2);
+Utility.GetInput(ref number1, ref number2, ref number3);
+
+// Start the stopwatch
+stopwatch.Start();
 
 // Call the GCD method
 result = Utility.GreatestCommonDivisor(number1, number2);
 
-// Print the result to the user
-Console.WriteLine($"The GCD of {number1} and {number2} is {result}.");
+if (number3 != 0)
+{
+    // Call the GCD method with the third number
+    result = Utility.GreatestCommonDivisor(result, number3);
+
+    // Stop the stopwatch
+    stopwatch.Stop();
+
+    // Print the result to the user
+    Console.WriteLine($"The GCD of {number1} and {number2} and {number3} is {result}.");
+
+    // Print the time taken to calculate the GCD using recursion
+    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to calculate the GCD of {number1}, {number2}, and {number3} using recursion.");
+
+    // Restart the stopwatch to track the time taken to calculate the GCD using iteration
+    stopwatch.Restart();
+
+    // Call the GCD Iterative method
+    result = Utility.GreatestCommonDivisorIterative(number1, number2);
+
+    // Call the GCD Iterative method with the third number
+    result = Utility.GreatestCommonDivisorIterative(result, number3);
+
+    // Stop the stopwatch
+    stopwatch.Stop();
+
+    // Print the result to the user
+    Console.WriteLine($"The GCD of {number1} and {number2} and {number3} from iterating is {result}.");
+
+    // Print the time taken to calculate the GCD using iteration
+    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to calculate the GCD of {number1}, {number2}, and {number3} using iteration.");
+}
+else
+{
+    // Stop the stopwatch
+    stopwatch.Stop();
+
+    // Print the result to the user
+    Console.WriteLine($"The GCD of {number1} and {number2} is {result}.");
+
+    // Print the time taken to calculate the GCD using recursion
+    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to calculate the GCD of {number1} and {number2} using recursion.");
+
+    // Restart the stopwatch to track the time taken to calculate the GCD using iteration
+    stopwatch.Restart();
+
+    // Call the GCD Iterative method
+    result = Utility.GreatestCommonDivisorIterative(number1, number2);
+
+    // Stop the stopwatch
+    stopwatch.Stop();
+
+    // Print the result to the user
+    Console.WriteLine($"The GCD of {number1} and {number2} from iterating is {result}.");
+
+    // Print the time taken to calculate the GCD using iteration
+    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to calculate the GCD of {number1} and {number2} using iteration.");
+}
 
 //--------------------------------------------------------
 // End of the Main Method
@@ -34,7 +97,7 @@ public class Utility
     /// <param name="num1"></param>
     /// <param name="num2"></param>
     /// <returns></returns>
-    internal static int GetInput(ref int num1, ref int num2)
+    internal static int GetInput(ref int num1, ref int num2, ref int num3)
     {
         // Declare and initialize
         string input = "";
@@ -70,7 +133,23 @@ public class Utility
 
         num2 = int.Parse(input);
 
-        return num2;
+        // Prompt the user for the second integer
+        Console.WriteLine("If you'd like to use a third number, enter it here, if not, input 0");
+        Console.Write("Please enter the third number: ");
+        // Get the current input
+        input = Console.ReadLine();
+        // Check if the input is valid
+        while (!int.TryParse(input, out output))
+        {
+            // Show the user an error message and prompt them again
+            Console.Write("Invalid input. Please try again: ");
+            // Get the new input
+            input = Console.ReadLine();
+        }
+
+        num3 = int.Parse(input);
+
+        return num3;
     }
 
 
@@ -97,5 +176,63 @@ public class Utility
             // Call the recursive function of the second number and the remainder
             return GreatestCommonDivisor(num2, remainder);
         }
+    }
+
+    internal static int GreatestCommonDivisorIterative(int num1, int num2)
+    {
+        int largestMatchingDivisor = 0;
+        // Create lists to hold the divisors of each number
+        List<int> num1Divisors = new List<int>();
+
+        List<int> num2Divisors = new List<int>();
+
+        // Print an update to the user
+        Console.WriteLine("Iterating to find the divisors of the first number...");
+
+        // Loop through each number from the first list and check if it is a divisor
+        for (int i = 1; i <= num1; i++)
+        {
+            // If the number is a divisor, add it to the list
+            if (num1 % i == 0)
+            {
+                // Add the divisor to the list
+                num1Divisors.Add(i);
+            }
+        }
+
+        // Print an update to the user
+        Console.WriteLine("Iterating to find divisors of the second number...");
+
+        // Loop through each number from the second list and check if it is a divisor
+        for (int i = 1; i <= num2; i++)
+        {
+            // If the number is a divisor, add it to the list
+            if (num2 % i == 0)
+            {
+                // Add the divisor to the list
+                num2Divisors.Add(i);
+            }
+        }
+
+        // Print an update to the user
+        Console.WriteLine("Looking for the largest matching divisor between the two...");
+
+        // Find the common elements between the two lists
+        var commonElements = num1Divisors.Intersect(num2Divisors);
+
+        // If there are common elements, find the largest one
+        if (commonElements.Any())
+        {
+            // Get the largest common element
+            largestMatchingDivisor = commonElements.Max();
+        }
+        else
+        {
+            // If there are no common elements, print a message to the user
+            Console.WriteLine("No matching values found between the two lists.");
+        }
+
+        // Return the largest common element
+        return largestMatchingDivisor;
     }
 }
