075e6c0902690f3252bac04ef44bf81398f2f37f
Etendo <etendo123@gmail.com>
2026-03-16 20:15:00 -0600

    Added user input

--- GreatestCommonDivisorRecursion/Program.cs
+++ GreatestCommonDivisorRecursion/Program.cs
@@ -11,7 +11,10 @@
 //--------------------------------------------------------
 
 // Declare and initialize
-int number1 = 440, number2 = 90, result = 0;
+int number1 = 0, number2 = 0, result = 0;
+
+// Call the GetInput method
+Utility.GetInput(ref number1, ref number2);
 
 // Call the GCD method
 result = Utility.GreatestCommonDivisor(number1, number2);
@@ -25,12 +28,61 @@ Console.WriteLine($"The GCD of {number1} and {number2} is {result}.");
 
 public class Utility
 {
-    internal static int GreatestCommonDivisor(int num1, int num2)
+    /// <summary>
+    /// Get the user imputs
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    internal static int GetInput(ref int num1, ref int num2)
     {
         // Declare and initialize
-        int remainder = 0;
+        string input = "";
+        int output = 0;
 
-        // Base case: num2 is 0
+        // Prompt the user for the first integer
+        Console.Write("Please enter the first number: ");
+        // Get the current input
+        input = Console.ReadLine();
+        // Check if the input is valid
+        while (!int.TryParse(input, out output))
+        {
+            // Show the user an error message and prompt them again
+            Console.Write("Invalid input. Please try again: ");
+            // Get the new input
+            input = Console.ReadLine();
+        }
+        // Parse the input to an integer
+        num1 = int.Parse(input);
+
+        // Prompt the user for the second integer
+        Console.Write("Please enter the second number: ");
+        // Get the current input
+        input = Console.ReadLine();
+        // Check if the input is valid
+        while (!int.TryParse(input, out output))
+        {
+            // Show the user an error message and prompt them again
+            Console.Write("Invalid input. Please try again: ");
+            // Get the new input
+            input = Console.ReadLine();
+        }
+
+        num2 = int.Parse(input);
+
+        return num2;
+    }
+
+
+    /// <summary>
+    /// Calculate the GCD using recursion
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    internal static int GreatestCommonDivisor(int num1, int num2)
+    {
+        int remainder = 0;
         if (num1 == 0 || num2 == 0)
         {
             // Return the greatest common divisor
